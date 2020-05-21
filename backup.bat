df03b76a9fbe0c2eb95854b877b7dfd7906da3ae
green1052 <34261355+green1052@users.noreply.github.com>
2020-05-21 09:35:56 +0900

    코드 최적화

--- backup.bat
+++ backup.bat
@@ -6,12 +6,15 @@ title 다크소울 3 세이브 자동 백업 프로그램 by green1052 V1.0.2
 
 :main
 cls
+
 if not exist "백업" (
 	mkdir "백업" > nul
 	echo "백업" 폴더가 없어 백업 폴더를 새로 생성 했습니다.
 	timeout 3 > nul
 )
+
 cls
+
 echo 메뉴 V1.0.2
 echo 1. 백업
 echo 2. 복원
@@ -28,14 +31,17 @@ if "%input%" == "3" goto remove
 if "%input%" == "4" exit
 
 cls
-echo 잘못된 입력
+
+echo 입력이 잘못 됐습니다.
 timeout 3 > nul
 goto main
 
 :backup
 cls
 xcopy /S /C /Y "%appdata%\DarkSoulsIII\*.sl2" "백업" > nul
+
 echo 세이브 파일이 "백업" 폴더에 백업 됐습니다.
+
 timeout 3 > nul
 goto main
 
@@ -48,15 +54,21 @@ for /f "delims=" %%A in ('dir /S /B "백업\*.sl2"') do (
 
 if "%files%" == "" (
 	cls
+	
 	echo "백업" 폴더에 백업 파일이 없습니다. 메뉴에서 백업을 한뒤 다시 시도해주세요.
+	
 	timeout 3 > nul
 	goto main
 )
 
 echo "백업" 폴더에 있는 모든 세이브를 적용 중 입니다.
+
 xcopy /S /C /Y "백업\*.sl2" "%appdata%\DarkSoulsIII" > nul
+
 echo 적용이 됐습니다. "백업" 폴더의 세이브를 지우고 있습니다.
+
 rd /S /Q "백업" > nul
+
 echo 삭제 완료
 
 timeout 3 > nul
@@ -66,5 +78,6 @@ goto main
 cls
 rd /S /Q "백업" > nul
 echo 저장된 백업 데이터를 전부 삭제했습니다.
+
 timeout 3 > nul
 goto main
--- q_backup.bat
+++ q_backup.bat
@@ -1,1 +1,5 @@
-xcopy /S /C /Y "%appdata%\DarkSoulsIII\*.sl2" "백업" > nul
+if not exist "백업" (
+	mkdir "백업"
+)
+
+xcopy /S /C /Y "%appdata%\DarkSoulsIII\*.sl2" "백업"
